9361a23f819208a1f3356c1887ca030b9652c2bd
Caleb <Caleb@10.255.96.248>
2018-10-26 12:54:59 -0400

    new commit

--- src/main/java/org/packt/primeautosales2/jsf/MainViewController.java
+++ src/main/java/org/packt/primeautosales2/jsf/MainViewController.java
@@ -5,6 +5,8 @@
  */
 package org.packt.primeautosales2.jsf;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import org.primefaces.event.ResizeEvent;
@@ -18,6 +20,7 @@ import org.primefaces.event.ResizeEvent;
 public class MainViewController {
     
     private String selectionType;
+    private List<objct> objects = new ArrayList();
     /**
      * Creates a new instance of MainViewController
      */
@@ -55,6 +58,33 @@ public class MainViewController {
     public String navigateToA3(){
         selectionType="specification3";
         return "automobile";
+    }
+
+    /**
+     * @return the objects
+     */
+    public List<objct> getObjects() {
+        objct object1= new objct();      
+        object1.setInfo1("String");
+        object1.setInfo2("String");
+        objct object2= new objct();      
+        object2.setInfo1("String");
+        object2.setInfo2("String");
+        objct object3= new objct();      
+        object3.setInfo1("String");
+        object3.setInfo2("String");
+        objects=new ArrayList();
+        objects.add(object1);
+        objects.add(object2);
+        objects.add(object3);
+        return objects;
+    }
+
+    /**
+     * @param objects the objects to set
+     */
+    public void setObjects(List<objct> objects) {
+        this.objects = objects;
     }
     
 }
