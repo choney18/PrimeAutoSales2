5556dbbed0236dd052f1d141408865ead28ee80e
Caleb <Caleb@10.255.96.248>
2018-10-26 11:23:35 -0400

    new commit

--- src/main/java/org/packt/primeautosales2/jsf/MainViewController.java
+++ src/main/java/org/packt/primeautosales2/jsf/MainViewController.java
@@ -16,7 +16,8 @@ import org.primefaces.event.ResizeEvent;
 @Named(value = "mainViewController")
 @RequestScoped
 public class MainViewController {
-
+    
+    private String selectionType;
     /**
      * Creates a new instance of MainViewController
      */
@@ -25,6 +26,35 @@ public class MainViewController {
     
     public void layoutResizeEvent (ResizeEvent event){
         System.out.println("The layout has been sized: " + event.getComponent());
+    }
+
+    /**
+     * @return the selectionType
+     */
+    public String getSelectionType() {
+        return selectionType;
+    }
+
+    /**
+     * @param selectionType the selectionType to set
+     */
+    public void setSelectionType(String selectionType) {
+        this.selectionType = selectionType;
+    }
+    
+    public String navigateToA1(){
+        selectionType="specification1";
+        return "automobile";
+    }
+    
+    public String navigateToA2(){
+        selectionType="specification2";
+        return "automobile";
+    }
+    
+    public String navigateToA3(){
+        selectionType="specification3";
+        return "automobile";
     }
     
 }
